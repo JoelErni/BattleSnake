afc3d513352bc14ed81ea8324c169fd788d05f49
Joel Erni <joel.erni@hispeed.ch>
2022-10-28 15:30:17 +0200

    Aid

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
-    print(f"headpos: {my_head}")
+    print(f"headpos: X:{my_head['x']}, Y:{my_head['y']}")
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
