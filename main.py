57bf3277e423f4b8c959f0563b713d344e538e60
Joel Erni <joel.erni@hispeed.ch>
2022-10-29 04:18:43 +0200

    asd

--- main.py
+++ main.py
@@ -154,9 +154,6 @@ def move(game_state: typing.Dict) -> typing.Dict:
                         map1[x] = 'b'
                         break
         map.append(map1)
-    flippedmap=[]
-    for x in reversed(map):
-        flippedmap.append(x)
 
     def mapoutput(mapinput) -> str:
         output = ""
@@ -178,7 +175,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
             if y < len(matrix) - 1:
                 floodfill(matrix,x,y+1)
 
-    floodfill(map)
+    floodfill(map, my_head['x'], my_head['y'])
     print(mapoutput(map))
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-a = [[1,2,3],[2,3,1],[3,1,2]]
-
-
-output = ""
-for x in range(len(a)):
-    for y in range(len(a[x])):
-        output = output + str(a[x][y])
-    output = output + "\n"
-print(output)
+map = []
+for y in reversed(range(11)):
+    map1 = ['a'] * 11
+    for x in range(11):
+        if x & 2 == 0:
+            map1[x] = 'b'
+            break
+    map.append(map1)
+print(map)
